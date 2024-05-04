b3405b8c8b45f1ef2b3f8a2ddf2258d7a5df4b5a
GearyAtHome2 <mgeary1994@gmail.com>
2024-05-04 17:57:15 +0100

    seems it might have just failed for horse - try this??

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
             * (max - min + 1)) + min;
     };
 
-    const image0=require("./public/horse.jpg")
+    const image0=require("/public/horse.jpg")
     const image1=require("/public/glassGrind.png")
 
     let imageUrl='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSiq57UyI71H6Lz2isMdomE4z2JNekyXhlxnoMpMgbRDAkVG2R5'
